5d4187d7f0d54a05e43ec7798939519571f27e11
tanhua <tanhua_0201@youzan.com>
2024-01-08 19:40:20 +0800

    feat:BusinessException.from_error

--- clio/web/exception/business_exception.py
+++ clio/web/exception/business_exception.py
@@ -7,6 +7,10 @@ class BusinessException(Exception):
         self.message = message
         self.cause = cause
 
+    @classmethod
+    def from_error(cls, error):
+        return cls(error.code, error.message)
+
     def __str__(self):
         if self.cause is None:
             return f"BusinessException, code: {self.code}, message: {self.message}"
